901764ccfaa84d4bc17ba1a10cf0985e33c314c6
GusevAlexsandr <25sprint25trap25@gmail.com>
2024-06-10 00:38:38 +0300

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,27 @@
+let footer = `
+<div class="content-ss">
+    Источники:<br>
+    <a href="https://ru.wikipedia.org/wiki/Пётр_I" target="_blank">Пётр I</a><br>
+    <a href="https://histrf.ru/read/biographies/pietr-i" target="_blank">Биография Петра I</a><br>
+    <a href="https://foxford.ru/wiki/okruzhayuschiy-mir/pyotr-velikiy?utm_referrer=https%3A%2F%2Fyandex.ru%2F" target="_blank">Пётр Великий</a><br>
+    <a href="https://ru.wikipedia.org/wiki/Память_о_Петре_I#На_деньгах" target="_blank">Пямять о Петре I</a>
+</div>
+<div class="form">
+    <form>
+        <div class="search">
+            <input type="text" name="email" class="contact_form" placeholder="Подпишитесь на новые публикации по e-mail">
+            <input type="button" value="Подписаться" class="button">
+        </div>
+    </form>
+</div>
+<div class="name">Гусев Александр Евгеньевич, 2024.</div>
+<div class="contact-ss">
+    <a href="https://vk.com/sprintrap25" target="_blank"><img src="img/footer/vk.png"
+        class="logotip"></a>
+    <a href="https://t.me/SprintrapOnamarh" target="_blank"><img src="img/footer/tg.png"
+        class="logotip"></a>
+    <a href="mailto:aeguse@chsu.ru" target="_blank"><img src="img/footer/gmail.png" class="logotip"></a>
+    <a href="https://www.chsu.ru" target="_blank"><img src="img/footer/chsu.png" class="logotip"></a>
+</div>
+`;
+document.getElementById("f_site").innerHTML = footer;
---
+++ js/header.js
@@ -0,0 +1,21 @@
+let header = `
+<a href="#win1" ></a>
+<a href="#x" class="overlay" id="win1"></a>
+    <div class="popup">
+        <div id="phrase"></div>
+    </div>
+<nav>
+    <ul>
+        <li><a href="index.html">Главная</a></li>
+        <li><a href="biography.html">Биография</a></li>
+        <li><a>Наследие &bigtriangledown;</a>
+            <ul>
+                <li><a href="Petersburg.html">Санкт-Петербург</a></li>
+                <li><a href="Image.html">Художественный образ</a></li>
+                <li><a href="coins.html">Монеты и марки</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+`;
+document.getElementById("h_site").innerHTML = header;
